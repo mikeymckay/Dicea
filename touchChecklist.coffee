819a1660073e7b7fc8ec32a653f0c62f26bd2aa0
Mike McKay <mikeymckay@gmail.com>
2011-01-06 00:28:28 -0500

    Added page() method to force a jquery mobile refresh

--- touchChecklist.coffee
+++ touchChecklist.coffee
@@ -1,4 +1,5 @@
 jQuery.getJSON "testsCallbackData.json", (data) ->
   checklist = new Checklist()
   checklist.loadFromGoogleSpreadhseet(GoogleSpreadsheet.callback(data))
-  $("div[data-role='content']").html(checklist.jqueryMobileForm())
+  $("div[data-role='content']").html(checklist.jqueryMobileForm()).page()
+
--- touchChecklist.js
+++ touchChecklist.js
@@ -2,5 +2,5 @@ jQuery.getJSON("testsCallbackData.json", function(data) {
   var checklist;
   checklist = new Checklist();
   checklist.loadFromGoogleSpreadhseet(GoogleSpreadsheet.callback(data));
-  return $("div[data-role='content']").html(checklist.jqueryMobileForm());
+  return $("div[data-role='content']").html(checklist.jqueryMobileForm()).page();
 });
